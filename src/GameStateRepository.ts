81d8f5e44950bff911250ca3fee9ca34a0f237f3
Valerij Dobler <vd@wps.de>
2024-11-13 17:56:09 +0100

    Adds save state version for safe invalidation

--- src/GameStateRepository.ts
+++ src/GameStateRepository.ts
@@ -1,27 +1,37 @@
 import { GameState } from "./GameState.js";
 
 export class GameStateRepository {
+    private version: string = "0.1.0";
+
     public save(gameState: GameState) {
         let payload = JSON.stringify(gameState);
         localStorage.setItem("save", payload)
+        localStorage.setItem("save-version", this.version);
     }
 
     public load(): GameState {
         console.log("loading");
         const saved = localStorage.getItem("save")
+        const version = localStorage.getItem("save-version")
         console.log("read from localstorage");
-        if (saved === null) throw new Error("No save present");
+        if (saved === null || version === null) throw new Error("No save present");
+        if (!this.compatible(version)) throw new Error(`${version} is not compatible with ${this.version}`);
 
-        let parse = JSON.parse(saved);
-        return parse;
+        return JSON.parse(saved);
     }
 
     public savePresent() {
         const saved = localStorage.getItem("save")
-        return saved !== null;
+        const version = localStorage.getItem("save-version")
+        return saved !== null && version !== null;
     }
 
     deleteSave() {
         localStorage.removeItem("save");
+        localStorage.removeItem("save-version");
+    }
+
+    compatible(readVersion: string): boolean {
+        return readVersion == this.version;
     }
 }
